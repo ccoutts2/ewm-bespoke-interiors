f8fb7aaf55af8187d281352cbcb92db95d85faf3
Chris Coutts <chris.dcoutts@gmail.com>
2024-05-29 17:06:48 +0100

    Add basic structure to what we do page

--- src/app/what-we-do/page.tsx
+++ src/app/what-we-do/page.tsx
@@ -2,12 +2,87 @@ import { assetsConfig } from "@/config/assets";
 
 import PagesHero from "@/components/PagesHero/PagesHero";
 
-const Page = () => {
+interface WorkProps {
+  src: string;
+  header: string;
+}
+
+const works = [
+  {
+    src: assetsConfig.officePicture.src,
+    header: "carpentry & bespoke joinery",
+  },
+  {
+    src: assetsConfig.officePicture.src,
+    header: "in house manufacturing of joinery",
+  },
+  {
+    src: assetsConfig.officePicture.src,
+    header: "in house spraying & upholsery",
+  },
+  {
+    src: assetsConfig.officePicture.src,
+    header: "stone surfaces",
+  },
+  {
+    src: assetsConfig.officePicture.src,
+    header: "timber repairs",
+  },
+  {
+    src: assetsConfig.officePicture.src,
+    header: "furniture supply & installation",
+  },
+];
+
+const Page = ({ src, header }: WorkProps) => {
   return (
     <section>
       <PagesHero img={assetsConfig.officePicture.src} header={"What We Do"} />
+      <section className="bg-[#e4e8ed] p-4 md:px-12 md:text-lg lg:px-32">
+        <Paragraphs
+          text="EWM Bespoke Interiors is a leading
+          provider of joinery, carpentry, upholstery / re-upholstery and office
+          furniture solutions for commercial and residential spaces. Our mission
+          is to deliver professional services, our team of experts work closely
+          with clients from concept to handover to ensure their expectations are
+          met."
+        />
+        <Paragraphs
+          text="We are a family run business with over 40 years of experience in the
+          industry, based in South East London"
+        />
+        <Paragraphs
+          text="As a family run business, we are invested in giving our clients a
+          high-quality service. Having a team that has come from the varied
+          backgrounds such as main contracting and production of carpentry,
+          upholstery and furniture installation gives us the necessary
+          experience to deliver high quality products."
+        />
+        <Paragraphs
+          text="Our workshop consists of all the relevant machinery required to
+          produce exceptional joinery items, spraying and upholstery to any
+          level of specification."
+        />
+      </section>
+      <section className="flex flex-col items-center justify-between bg-[#e4e8ed] px-4 py-8 md:flex-row md:flex-wrap md:gap-4 md:px-12 lg:px-32">
+        {works.map((work, index) => (
+          <div
+            key={index}
+            className="flex w-full flex-col items-center justify-center py-4 capitalize md:w-[45%] lg:w-[30%]"
+          >
+            <div>
+              <img src={work.src}></img>
+            </div>
+            <h3 className="py-2 text-[1rem]">{work.header}</h3>
+          </div>
+        ))}
+      </section>
     </section>
   );
 };
 
 export default Page;
+
+const Paragraphs = (props: { text: string }) => {
+  return <p className="py-4 text-[#191919]">{props.text}</p>;
+};
--- src/components/PagesHero/PagesHero.tsx
+++ src/components/PagesHero/PagesHero.tsx
@@ -1,7 +1,9 @@
 const PagesHero = (props: { img: string; header: string }) => {
   return (
-    <section className="relative z-[-1] mt-[1rem] flex h-[60vh] w-full items-center justify-center">
-      <h1 className="absolute z-[1] text-3xl text-[white]">{props.header}</h1>
+    <section className="relative z-[-1] mt-[1rem] flex h-[60vh] w-full items-center justify-center md:mt-0 lg:h-[75vh] ">
+      <h1 className="absolute z-[1] text-4xl text-[white] md:text-5xl">
+        {props.header}
+      </h1>
       <img
         src={props.img}
         className="h-full w-full object-cover brightness-[0.6]"
