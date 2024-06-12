b0e424b7d71f18beda1452cece74adad216690a1
Chris Coutts <chris.dcoutts@gmail.com>
2024-06-12 12:10:06 +0100

    Add image carousel to hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,74 +1,86 @@
 "use client";
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
 
-import Link from "next/link";
+// import Link from "next/link";
 import { assetsConfig } from "@/config/assets";
+const mewsPropertyKey: "mews-property" = "mews-property";
+const hero1 = assetsConfig[mewsPropertyKey][5].src;
+const hero2 = assetsConfig[mewsPropertyKey][6].src;
+const hero3 = assetsConfig[mewsPropertyKey][7].src;
+
+import ImageCarousel from "../ImageCarousel/ImageCarousel";
 
 const Hero = () => {
-  const scrollRef = useRef<HTMLElement | null>(null);
+  // const scrollRef = useRef<HTMLElement | null>(null);
+
+  const images = [hero1, hero2, hero3];
 
   return (
-    <section ref={scrollRef} className="relative bg-[#e4e8ed]">
-      <Section1 />
-      <Section2 />
+    <section
+      // ref={scrollRef}
+      className="relative bg-[#e4e8ed]"
+    >
+      {/* <Section1 />
+      <Section2 /> */}
+      <ImageCarousel images={images} auto />
     </section>
   );
 };
 
 export default Hero;
 
-const Section1 = () => {
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const locomotiveScroll = new LocomotiveScroll();
-    })();
-  }, []);
+// const Section1 = () => {
+//   useEffect(() => {
+//     (async () => {
+//       const LocomotiveScroll = (await import("locomotive-scroll")).default;
+//       const locomotiveScroll = new LocomotiveScroll();
+//     })();
+//   }, []);
 
-  return (
-    <section className="sticky top-0 flex h-screen flex-col items-center justify-center">
-      <div className="h-screen w-full">
-        <img
-          src={assetsConfig.officePicture[0].src}
-          alt={assetsConfig.officePicture[0].description}
-          className="h-full w-full object-cover brightness-[0.6]"
-        />
-      </div>
-      <div className="absolute top-[15rem]">
-        <h1
-          data-scroll
-          data-scroll-speed="-0.4"
-          className=" px-4 text-4xl capitalize text-white md:px-8 md:text-6xl"
-        >
-          crafted by hand. created by experts
-        </h1>
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section className="sticky top-0 flex h-screen flex-col items-center justify-center">
+//       <div className="h-screen w-full">
+//         <img
+//           src={assetsConfig.officePicture[0].src}
+//           alt={assetsConfig.officePicture[0].description}
+//           className="h-full w-full object-cover brightness-[0.6]"
+//         />
+//       </div>
+//       <div className="absolute top-[15rem]">
+//         <h1
+//           data-scroll
+//           data-scroll-speed="-0.4"
+//           className=" px-4 text-4xl capitalize text-white md:px-8 md:text-6xl"
+//         >
+//           crafted by hand. created by experts
+//         </h1>
+//       </div>
+//     </section>
+//   );
+// };
 
-const Section2 = () => {
-  return (
-    <section className="relative h-screen bg-[#e4e8ed]">
-      <div className="absolute top-[3rem] px-4 text-[#191919] md:top-[2rem] md:w-[80%] md:px-12 md:pt-12 lg:p-32">
-        <p className="py-4 text-lg md:text-3xl">
-          <span className="uppercase">ewm</span> Bespoke Interiors is a leading
-          provider of joinery, carpentry, upholstery / re-upholstery and office
-          furniture solutions for commercial and residential spaces.
-        </p>
-        <p className="py-4 text-lg md:text-3xl">
-          We are a family run business with over 40 years of experience in the
-          industry, based in South East London.
-        </p>
+// const Section2 = () => {
+//   return (
+//     <section className="relative h-screen bg-[#e4e8ed]">
+//       <div className="absolute top-[3rem] px-4 text-[#191919] md:top-[2rem] md:w-[80%] md:px-12 md:pt-12 lg:p-32">
+//         <p className="py-4 text-lg md:text-3xl">
+//           <span className="uppercase">ewm</span> Bespoke Interiors is a leading
+//           provider of joinery, carpentry, upholstery / re-upholstery and office
+//           furniture solutions for commercial and residential spaces.
+//         </p>
+//         <p className="py-4 text-lg md:text-3xl">
+//           We are a family run business with over 40 years of experience in the
+//           industry, based in South East London.
+//         </p>
 
-        <div className="py-4">
-          <Link className="no-underline" href="/about">
-            <button className=" cursor-pointer rounded-[20rem] bg-[#191919] px-4 py-2 capitalize text-[#e4e8ed] md:px-8 md:py-4 md:text-2xl">
-              our story
-            </button>
-          </Link>
-        </div>
-      </div>
-    </section>
-  );
-};
+//         <div className="py-4">
+//           <Link className="no-underline" href="/about">
+//             <button className=" cursor-pointer rounded-[20rem] bg-[#191919] px-4 py-2 capitalize text-[#e4e8ed] md:px-8 md:py-4 md:text-2xl">
+//               our story
+//             </button>
+//           </Link>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
---
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -0,0 +1,72 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+type ImageCarouselProps = {
+  images: string[];
+  auto?: boolean;
+  interval?: number;
+};
+
+const ImageCarousel = ({
+  images,
+  auto = false,
+  interval = 5000,
+}: ImageCarouselProps) => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const showNextImage = () => {
+    setImageIndex((index) => {
+      if (index === images.length - 1) return 0;
+      return index + 1;
+    });
+  };
+
+  useEffect(() => {
+    if (auto) {
+      const intervalImage = setInterval(showNextImage, interval);
+      return () => clearInterval(intervalImage);
+    }
+  }, []);
+
+  return (
+    <div className="relative h-full w-full">
+      <div className="flex h-[50vh] w-full overflow-hidden lg:h-[85vh]">
+        {images.map((url, index) => (
+          <Image
+            width={2000}
+            height={2000}
+            key={url}
+            src={url}
+            alt="carousel"
+            className="absolute left-0 top-0 block h-full w-full shrink-0 grow-0 object-cover"
+            style={{
+              transition: "opacity 1s ease-in-out",
+              opacity: index === imageIndex ? 1 : 0,
+            }}
+          />
+        ))}
+      </div>
+      <div className="absolute bottom-2 left-1/2 flex -translate-x-1/2 transform gap-1">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            className={`
+            relative mx-[6px] inline-block h-[9px] w-[9px] cursor-pointer rounded-full border [transition:color_0.2s_ease-in-out]
+              ${
+                index === imageIndex
+                  ? "border-[#d9c6c5] bg-[#d9c6c5]"
+                  : "border-[#d9c6c5] bg-transparent"
+              }
+            `}
+            onClick={() => setImageIndex(index)}
+          >
+            <span aria-hidden="true"></span>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageCarousel;
