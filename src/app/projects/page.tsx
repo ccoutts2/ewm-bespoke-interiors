14f7b6fedc4071ec325c9e6aa1e501137441e3bd
Chris Coutts <chris.dcoutts@gmail.com>
2024-07-09 18:51:50 +0100

    Remove unwanted styling from mobile-styling/feat

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -92,92 +92,22 @@ const Page = ({ params: { projectId } }: ProjectPage) => {
     requestAnimationFrame(raf);
   }, []);
 
-  const container = useRef<HTMLElement>(null);
-  const gallery = useRef<HTMLDivElement>(null);
-
-  const images = useRef<(HTMLImageElement | null)[]>([]);
-
-  let rotationValues = [10, -5, 2, -2];
-
-  const [isFlipped, setIsFlipped] = useState(false);
-  const [buttonText, setButtonText] = useState("Explore Projects");
-
-  console.log(isFlipped);
-
-  const buttonFlip = () => {
-    setIsFlipped(!isFlipped);
-  };
-
-  useEffect(() => {
-    const delay = setTimeout(() => {
-      setButtonText(isFlipped ? "Collapse Projects" : "Explore Projects");
-    }, 1000);
-
-    return () => clearTimeout(delay);
-  }, [isFlipped]);
-
-  useGSAP(
-    () => {
-      buttonFlip();
-
-      gsap.to(images.current, {
-        x: "0",
-        y: "0",
-      });
-    },
-    { scope: container },
-  );
-
   return (
     <>
       <NavBar />
-      <section ref={container} className="h-screen">
-        <div ref={gallery} className="flex w-screen gap-16 px-32 pt-[25vh]">
+      <section className="flex items-center justify-center px-4 py-16 md:px-12 lg:px-32">
+        <div className="flex flex-wrap items-center justify-center lg:gap-8">
           {projects.map((project, index) => (
-            <div
+            <Projects
               key={index}
-              className="m-b-[1em] relative h-[500px] w-[400px] overflow-hidden"
-            >
-              <img
-                ref={(e) => {
-                  if (e) images.current[index] = e;
-                }}
-                src={project.src}
-                alt={project.alt}
-              />
-            </div>
+              href={project.href}
+              src={project.src}
+              title={project.title}
+            />
           ))}
         </div>
       </section>
-      <button
-        onClick={buttonFlip}
-        className="absolute left-1/2 top-[75%] z-[2] -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-3xl bg-[#0084ff] px-[2em] py-[1em] text-white"
-      >
-        {buttonText}
-      </button>
-      <section>
-        {/* <nav className="mt-[4rem] px-4 py-4 md:mt-0 md:py-8 lg:px-32">
-          <ul className="flex w-full flex-row flex-wrap items-stretch justify-center no-underline lg:justify-between">
-            {projectNavItems.map((item, index) => (
-              <Link href={item.href} key={index} className="no-underline">
-                <li className="list-none uppercase">{item.title}</li>
-              </Link>
-            ))}
-          </ul>
-        </nav> */}
-        <section className="flex items-center justify-center px-4 py-16 md:px-12 lg:px-32">
-          <div className="flex flex-wrap items-center justify-center lg:gap-8">
-            {projects.map((project, index) => (
-              <Projects
-                key={index}
-                href={project.href}
-                src={project.src}
-                title={project.title}
-              />
-            ))}
-          </div>
-        </section>
-      </section>
+
       <Footer />
     </>
   );
