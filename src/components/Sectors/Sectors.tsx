963dbd1bb27ebd480a18b374b44bcaeec8840617
Chris Coutts <chriscoutts@Chriss-MacBook-Pro.local>
2024-07-09 10:52:42 +0100

    Remove two sectors and edit sector info

--- src/components/Sectors/Sectors.tsx
+++ src/components/Sectors/Sectors.tsx
@@ -5,32 +5,22 @@ const sectors = [
   {
     src: assetsConfig.officePicture[0].src,
     header: "residential",
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quos, optio ducimus quisquam temporibus voluptate obcaecati",
+    info: "Custom design kitchens, wardrobes, media walls and storage to fit your home spaces perfectly",
   },
   {
     src: assetsConfig.officePicture[0].src,
     header: "commerical",
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quos, optio ducimus quisquam temporibus voluptate obcaecati",
-  },
-  {
-    src: assetsConfig.officePicture[0].src,
-    header: "retail",
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quos, optio ducimus quisquam temporibus voluptate obcaecati",
-  },
-  {
-    src: assetsConfig.officePicture[0].src,
-    header: "hospitality",
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quos, optio ducimus quisquam temporibus voluptate obcaecati",
+    info: "From reception desks to banquette seating, our experience in the retail sector is extensive and we manufacture a wide range of elements to complement your vision",
   },
   {
     src: assetsConfig.officePicture[0].src,
     header: "heritage & culture",
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quos, optio ducimus quisquam temporibus voluptate obcaecati",
+    info: "We understand that buildings with historic and cultural importance desersve and demand a certain approach when it comes to detail and specification. Our team have extensive experience in delivering projects within this sector from Grade I listed museums to high-end art galleries. Our works in these properties predominantly consist of bespoke joinery & carpentry",
   },
   {
     src: assetsConfig.officePicture[0].src,
     header: "cat a/b office fit out",
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quos, optio ducimus quisquam temporibus voluptate obcaecati",
+    info: "This is a sector we offer our widest range of services. It includes your traditional 1st & 2nd fix carpentry, bespoke joinery, vanity units and office furniture. We have a dedicated team for each element and would work closely with your team to provide the best solutions for your space",
   },
 ];
 
@@ -42,14 +32,13 @@ const Sectors = () => {
         {sectors.map((sector, index) => (
           <div
             key={index}
-            className="flex w-full flex-col justify-start py-4 capitalize md:w-[45%] lg:w-[30%]"
+            className="flex w-full flex-col justify-start py-4 capitalize md:w-[45%] lg:w-[40%]"
           >
             <div>
               <img src={sector.src}></img>
             </div>
             <h3 className="py-2 text-lg">{sector.header}</h3>
             <p className="pb-4 pt-2 text-sm leading-loose">{sector.info}</p>
-            <Button href="/" label={`explore ${sector.header} works`} />
           </div>
         ))}
       </div>
